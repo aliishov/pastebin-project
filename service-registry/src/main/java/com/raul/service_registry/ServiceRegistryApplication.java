4332dcfa0daf92c0c80ed15ac6dc8d53a6090668
aliishov <alishov7394@gmail.com>
2024-09-16 13:15:33 +0400

    Add eureka server

---
+++ service-registry/src/main/java/com/raul/service_registry/ServiceRegistryApplication.java
@@ -0,0 +1,14 @@
+package com.raul.service_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Enable
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+}
