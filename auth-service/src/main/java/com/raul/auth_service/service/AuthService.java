fc02fb19fbbe6e2216de022037c2f240165ca0c6
aliishov <alishov7394@gmail.com>
2025-02-11 11:59:41 +0400

    add swagger to services

--- auth-service/src/main/java/com/raul/auth_service/service/AuthService.java
+++ auth-service/src/main/java/com/raul/auth_service/service/AuthService.java
@@ -16,6 +16,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -146,6 +147,7 @@ public class AuthService {
         return new ResponseEntity<>(new MessageResponse("Password successfully reset"), HttpStatus.OK);
     }
 
+    @Transactional
     public ResponseEntity<MessageResponse> confirmEmail(String token) {
 
         Integer userId;
---
+++ user-service/src/main/java/com/example/user_service/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.example.user_service.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Raul Alishov",
+                        email = "alishov7394@gmail.com",
+                        url = "https://github.com/aliishov"
+                ),
+                description = "OpenApi Documentation",
+                title = "User Management Service",
+                version = "1.0"
+        )
+)
+public class SwaggerConfig {
+}
