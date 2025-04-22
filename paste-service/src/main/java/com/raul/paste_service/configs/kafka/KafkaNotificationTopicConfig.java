f25c5abcf77e9f47eaf1f430cfa76f595db223c0
aliishov <alishov7394@gmail.com>
2025-04-22 11:48:14 +0400

    Dockerized All Services

--- paste-service/src/main/java/com/raul/paste_service/configs/kafka/KafkaNotificationTopicConfig.java
+++ paste-service/src/main/java/com/raul/paste_service/configs/kafka/KafkaNotificationTopicConfig.java
@@ -19,7 +19,7 @@ public class KafkaNotificationTopicConfig {
     @Bean
     public ProducerFactory<String, EmailNotificationDto> emailProducerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
